5c2be4eb396dced596798e8a26a53ee144e10334
bellonet <bellonet@gmail.com>
2018-04-05 22:34:01 +0200

    automatic commit - bimsb_seminar

--- deploy_site.sh
+++ deploy_site.sh
@@ -2,10 +2,6 @@ cd /home/ella/preibisch-labsite;
 git add _extras/bimsb_seminar.html;
 git commit -m "automatic commit - bimsb_seminar";
 git push;
-
 git pull;
-
 jekyll build;
-echo "after jekyll"
-
 rsync -r --progress _site/* ebahry@mdcuserweb.mdc-berlin.net:/var/www/html/004_preibischlab/
