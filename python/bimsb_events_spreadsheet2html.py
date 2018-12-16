53c20b8b48befe961bac303393b7ddf9101efa2b
bellonet <bellonet@gmail.com>
2018-12-16 15:18:36 +0100

    bimsb events 2019

--- python/bimsb_events_spreadsheet2html.py
+++ python/bimsb_events_spreadsheet2html.py
@@ -73,6 +73,8 @@ for key,value in events.items():
 # Add events to HTML file:
 
 placeholders = [f'PLACEHOLDER_{key[7:].upper}' for key in events]
+print(placeholders)
+print(list_of_strings_4_html[0])
 
 f_read = open (script_folder + "bimsb_with_placeholder.txt", "r") 
 f_write = open (folder + "bimsb_seminar.html", "w")
